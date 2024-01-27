3256cf3dcd0334f593406983badfe8f2f08ff268
Russell Gordon <rgordon@lcs.on.ca>
2024-01-27 11:44:35 -0500

    PhotosPicker example works!

--- PhotoPickerExample/ContentView.swift
+++ PhotoPickerExample/ContentView.swift
@@ -5,17 +5,43 @@
 //  Created by Russell Gordon on 2024-01-27.
 //
 
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: Stored properties
+    @State private var landmarkItem: PhotosPickerItem?
+    @State private var landmarkImage: Image?
+    @State private var result: String = ""
+    
+    
+    // MARK: Computed properties
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            // SEE: https://www.hackingwithswift.com/quick-start/swiftui/how-to-let-users-select-pictures-using-photospicker
+            PhotosPicker(
+                "Select landmark",
+                selection: $landmarkItem,
+                matching: .images
+            )
+            .onChange(of: landmarkItem) {
+                Task {
+                    if let loaded = try? await landmarkItem?.loadTransferable(type: Image.self) {
+                        landmarkImage = loaded
+                        result = "Success"
+                    } else {
+                        result = "Failed"
+                    }
+                }
+            }
+            
+            landmarkImage?
+                .resizable()
+                .scaledToFit()
+            
+            Text("**Result:** \(result)")
         }
-        .padding()
     }
 }
 
